af3d7bc587ec02fc96299e0edae57549fa0431de
luispaulot <luispaulo1011@gmail.com>
2017-12-29 11:09:47 -0200

    change admin to administrativo-comunidade

--- comunidade/urls.py
+++ comunidade/urls.py
@@ -8,5 +8,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('homesite.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^administrativo-comunidade/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
